c099a5bd1287245740fd12685709a4ec89489c69
jeldo <101758793+xcvh@users.noreply.github.com>
2025-12-18 17:02:48 +0100

    fix: Prevent React Router from handling /admin path
    - Skip React app rendering when on /admin route
    - Allows Decap CMS to load as standalone HTML

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,16 @@ import App from "./App.jsx";
 import "flag-icons/css/flag-icons.min.css";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <I18nProvider>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </I18nProvider>
-  </React.StrictMode>,
-);
+// Don't render React app if we're on the admin page
+// The admin page is a standalone HTML file that loads Decap CMS
+if (!window.location.pathname.startsWith("/admin")) {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <I18nProvider>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </I18nProvider>
+    </React.StrictMode>,
+  );
+}
